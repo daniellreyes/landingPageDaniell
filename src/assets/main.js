aed2a4c175a747bc405ee11d847ab0cba960c6d1
daniell <daniell.reyes26@gmail.com>
2023-10-15 18:27:52 -0600

    Added Youtube API

---
+++ src/assets/main.js
@@ -0,0 +1,44 @@
+//const fetch = require('node-fetch');
+
+const content = null || document.getElementById('content');
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCmkBG6BKV1Yxbo40V81rwrA&part=snippet%2Cid&order=date&maxResults=2';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '3014bf7639mshe4c0732eb8b720bp1f8fddjsn3a2e9533f427',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com' // esta es la key privada, la cual no hay que mostar
+  }
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.tittle}
+            </h3>
+          </div>
+        </div>
+        `).slice(0, 4).join('')}
+
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+        //messageError(error);
+    }
+}) ();
